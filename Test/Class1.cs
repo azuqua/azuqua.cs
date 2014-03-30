c1b0ff5331024528d59e74619b740ae33f8aa550
Anusidh Pavamontri <apavamontri@gmail.com>
2014-03-29 23:43:24 -0700

    Empty library and test projects

---
+++ Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+using NUnit.Framework;
+
+using Azuqua;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void ShouldBeTrue()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}

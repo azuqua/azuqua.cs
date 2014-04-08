2f1d0a1a28a9c30894672f37ba312951a6226751
Anusidh Pavamontri <apavamontri@gmail.com>
2014-04-08 08:03:35 -0700

    ConfigReader: Read key and secret from environment variables

---
+++ Azuqua/ConfigReader.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ConfigReader.cs" company="Azuqua, Inc">
+//     Copyright (c) Azuqua, Inc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Azuqua
+{
+    using System;
+
+    /// <summary>
+    /// This class is responsible for reading Flo Key and Secret from
+    /// system environment variables or application configuration file
+    /// </summary>
+    public class ConfigReader
+    {
+        /// <summary>
+        /// Flo's access key environment variable name
+        /// </summary>
+        private const string FloAccessKeyVariableName = "floAccessKey";
+
+        /// <summary>
+        /// Flo's access secret environment variable name
+        /// </summary>
+        private const string FloAccessSecretVariableName = "floAccessSecret";
+
+        /// <summary>
+        /// Initializes a new instance of the ConfigReader class
+        /// </summary>
+        public ConfigReader()
+        {
+            this.LoadConfigurationData();
+        }
+
+        /// <summary>
+        /// Gets Flo API key
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <summary>
+        /// Gets Flo API secret
+        /// </summary>
+        public string Secret { get; private set; }
+
+        /// <summary>
+        /// Load Flo configuration data
+        /// </summary>
+        private void LoadConfigurationData()
+        {
+            this.Key = System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName);
+            this.Secret = System.Environment.GetEnvironmentVariable(FloAccessSecretVariableName);
+        }
+    }
+}
---
+++ Test/ConfigReaderTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+using NUnit.Framework;
+
+using Azuqua;
+
+namespace Azuqua.Test
+{
+    [TestFixture]
+    public class ConfigReaderTests
+    {
+        #region Have only environment variables
+
+        [Test]
+        public void ShouldReturnEmptyKeyAndSecretIfEnvironmentVariablesAreMissing()
+        {
+            ConfigReader reader = new ConfigReader();
+
+            Assert.IsTrue(String.IsNullOrEmpty(reader.Key));
+            Assert.IsTrue(String.IsNullOrEmpty(reader.Secret));
+        }
+
+        [Test]
+        public void ShouldReturnCorrectKeyAndSecretFromEnvironmentVariables()
+        {
+            string key = "aaaaa";
+            string secret = "bbbbb";
+
+            this.SetEnvironmentVariables(key, secret);
+
+            ConfigReader reader = new ConfigReader();
+
+            Assert.AreEqual(key, reader.Key);
+            Assert.AreEqual(secret, reader.Secret);
+
+            this.DeleteEnvironmentVariables();
+        }
+
+        #endregion
+
+        #region Have only configuration file
+
+        [Test]
+        public void ShouldReturnEmptyKeyAndSecretIfConfigFileIsMissing()
+        {
+            ConfigReader reader = new ConfigReader();
+
+            Assert.IsTrue(String.IsNullOrEmpty(reader.Key));
+            Assert.IsTrue(String.IsNullOrEmpty(reader.Secret));
+        }
+
+        [Test]
+        [Ignore]
+        public void ShouldReturnCorrectKeyAndSecretFromConfigurationFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Have both environment variables and configuration file
+
+        [Test]
+        [Ignore]
+        public void ShouldReturnKeyAndSecretFromConfigurationFileWhenEnvironmentVariablesAreMissing()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [Ignore]
+        public void ShouldReturnKeyAndSecrentFromEnvironmentVariablesWhenConfigurationFileIsMissing()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private void SetEnvironmentVariables(string key, string secret)
+        {
+            string key_name = "floAccessKey";
+            string secret_name = "floAccessSecret";
+
+            Environment.SetEnvironmentVariable(key_name, key);
+            Environment.SetEnvironmentVariable(secret_name, secret);
+        }
+        
+        private void DeleteEnvironmentVariables()
+        {
+            string key_name = "floAccessKey";
+            string secret_name = "floAccessSecret";
+
+            Environment.SetEnvironmentVariable(key_name, string.Empty);
+            Environment.SetEnvironmentVariable(secret_name, string.Empty);
+        }
+    }
+}

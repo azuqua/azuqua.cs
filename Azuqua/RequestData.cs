1b101453ea31e63f676c106a87c91d0b40c982ff
Anusidh Pavamontri <apavamontri@gmail.com>
2014-04-10 06:06:30 -0700

    RequestData: Add basic RequestData class

---
+++ Azuqua/RequestData.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RequestData.cs" company="Azuqua, Inc">
+//     Copyright (c) Azuqua, Inc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Azuqua
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This class encapsulates all necessary data that need to make
+    /// a request to Flo's API
+    /// </summary>
+    public class RequestData
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the RequestData class
+        /// </summary>
+        /// <param name="data">Data to be sent to Flo API</param>
+        public RequestData(string data) : this(string.Empty, string.Empty, data)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RequestData class
+        /// </summary>
+        /// <param name="key">Flo API key</param>
+        /// <param name="secret">Flo API secret</param>
+        /// <param name="data">Data to be sent to Flo API</param>
+        public RequestData(string key, string secret, string data)
+        {
+            this.Key = key;
+            this.Secret = secret;
+            this.Data = data;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets or sets Flo API key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets Flo API secret
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Gets or sets Data to be sent to Flo API
+        /// </summary>
+        public string Data { get; set; }
+    }
+}
---
+++ Test/RequestDataTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit;
+using NUnit.Framework;
+
+using Azuqua;
+
+namespace Azuqua.Test
+{
+    [TestFixture]
+    public class RequestDataTest
+    {
+        [Test]
+        public void CanCreateWithData()
+        {
+            string data = "";
+
+            RequestData requestData = new RequestData(data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public void CanCreateWithKeySecretAndData()
+        {
+            string key = "aaaaa";
+            string secret = "bbbbb";
+            string data = "";
+
+            RequestData requestData = new RequestData(key, secret, data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public void CanCreateWithEmptyKeySecret()
+        {
+            string key = "";
+            string secret = "";
+            string data = "ccc";
+
+            RequestData requestData = new RequestData(key, secret, data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public void ShouldSetAndGetPropertiesCorrectly()
+        {
+            string key = "aaa";
+            string secret = "bbb";
+            string data = "ccc";
+
+            RequestData requestData = new RequestData(key, secret, data);
+
+            Assert.AreEqual(key, requestData.Key);
+            Assert.AreEqual(secret, requestData.Secret);
+            Assert.AreEqual(data, requestData.Data);
+        }
+    }
+}

29b2616e4be5dcfdd110704db160c693eed290a0
Lito Nicolai <lito@azuqua.com>
2014-10-02 11:52:28 -0700

    Core finished- on to testing

--- Azuqua/MakeRequest.cs
+++ Azuqua/MakeRequest.cs
@@ -6,64 +6,82 @@
 
 using System;
 using System.Security.Cryptography;
+using System.Net;
+using System.Text;
+using System.IO;
 
 /// This class encapsulates all necessary data that need to make
 /// a request to Flo's API
 public class MainTest {
     // TODO: TESTING ONLY (Lito)
     static void Main(string[] args) {
-            // Display the number of command line arguments:
-            Azuqua azu = new Azuqua("key", "secret");
-            string timestamp = DateTime.UtcNow.ToString("s"); // ISO formatted date
-            string hash = azu.SignData("/account/flos", "get", "Hello World", timestamp);
-            System.Console.WriteLine(hash);
-        }
+        // Display the number of command line arguments:
+        Azuqua azu = new Azuqua("key", "secret");
+    }
 }
-// ???
-// How the heck do we match the time so exactly to unhash?
-
 
 public class Azuqua {
 
     private string accessKey, accessSecret;
+
     /// Constructor
     public Azuqua(string accessKey, string accessSecret) {
         this.accessKey = accessKey;
         this.accessSecret = accessKey;
     }
 
-    public void MakeRequest(string path, string verb, string data) {
-        verb = verb.ToUpper();
+    public string InvokeFlo(string name, string alias, string data) {
+        string path = "/flo/"+alias+"/invoke";
+        string resp = this.MakeRequest(path, "POST", data);
+        return resp;
+    }
+
+    private string MakeRequest(string path, string verb, string data) {
         if (data.Length < 1) {
             data = "";
         }
-        //Dictionary<string, string> headers = HTTP_headers;
+        //TODO: DOUBLE CHECK TIMESTAMP
         string timestamp = DateTime.UtcNow.ToString("s"); // ISO formatted date
+        string hash = this.SignData(path, verb, data, timestamp);
 
-        byte[] hash = this.SignData(path, verb, data, timestamp);
-
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(""); 
+        verb = verb.ToUpper();
+        string host = "https://api.azuqua.com";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host+path);
         request.Method = verb;
-        request.ContentLength = hash.Length;
-        request.ContentType = ; //TODO
-        // Let's take a second to appricate how cool the idea of streams is
-        Stream requestStream = request.GetRequestStream();
-        dataStream.Write(hash, 0, hash.Length);
+        request.ContentType = "application/json";
+        request.Headers.Add("x-api-accessKey", this.accessKey);
+        request.Headers.Add("x-api-timestamp", timestamp);
+        request.Headers.Add("x-api-hash", hash);
 
-        // Make it happen!
-        // TODO: In the ifs
-        HttpWebRequest response = request.GetResponse();
-
-        // string jsonBody = "{\"x-api-accessKey\": 
-
-        host = "https://api.azuqua.com"
+        // Make it happen! 
         if (verb == "GET") {
-            string address = "/account/flos";
-
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            using (var streamReader = new StreamReader(responseStream)) {
+                string responseText = streamReader.ReadToEnd();
+                return responseText;        
+            }
         }
-        else if (verb == "PUT") {
+        else if (verb == "POST") {
+            // Let's take a second to appricate how cool the idea of streams is
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            request.ContentLength = dataBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(dataBytes, 0, dataBytes.Length);
 
+            // .GetResponse both sends to the server and gets the response
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            using (var streamReader = new StreamReader(responseStream)) {
+                string responseText = streamReader.ReadToEnd();
+                return responseText;        
+            }
         }
+        else {
+            // Should never get here- requests should be only GET or POST
+            return ""; 
+        }
+
     }
 
     /// Generate Hash-based Message Authentication Code (HMAC) with 

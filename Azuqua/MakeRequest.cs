28ab01fbf4eb84b7072a2502a948e9d403b05e92
Lito Nicolai <lito.nicolai@gmail.com>
2014-10-01 22:29:12 -0700

    Finished Crypto

--- Azuqua/MakeRequest.cs
+++ Azuqua/MakeRequest.cs
@@ -7,62 +7,58 @@
 using System;
 using System.Security.Cryptography;
 
-/// Utility class to contain account info
-public class AzuquaAccount {
-    public AzuquaAccount(string accessKey, string accessSecret) {
-        AccessKey = accessKey;
-        AccessSecret = accessKey;
-    }
-    public string AccessKey {get; set;}
-    public string AccessSecret {get; set;}
-}
-
 /// This class encapsulates all necessary data that need to make
 /// a request to Flo's API
 public class Azuqua {
 
-    /// Configuration settings reader
-    private ConfigReader config = new ConfigReader();
+    /// TODO(Lito): Configuration settings reader
+    private string ROUTES_list_path = "/account/flos";
+    private string ROUTES_list_method = "get";
+    private string ROUTES_invoke_path = "/flo/:id/invoke";
 
-    private Dictionary<string, string> ROUTES = new Dictionary<string, string>() 
-    {
-        {"list", new Dictionary<string, string>()
-            {
-                {"path", "/flo/:id/invoke"},
-                {"method", "post"}
-            }
-        }
-    }
-
+    private string accessKey, accessSecret;
     /// Constructor
     public Azuqua(string accessKey, string accessSecret) {
         this.accessKey = accessKey;
         this.accessSecret = accessKey;
     }
 
-    public void MakeRequest() {
-        client = HTTP
+    public void MakeRequest(string path, string verb, string data) {
+        if (data.Length < 1) {
+            data = "";
+        }
+        Dictionary<string, string> headers = HTTP_headers;
+        string timestamp = DateTime.UtcNow.ToString("s"); // ISO formatted date
 
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dcrypt.it/decrypt/paste"); 
+        //TODO: fixme
+        string hash = this.SignData(data, verb, path, timestamp);
+
+        if (verb == "get") {
+
+        }
+        else if (verb == "put") {
+
+        }
     }
 
     /// Generate Hash-based Message Authentication Code (HMAC) with 
     /// SHA256 and secret key
-    private string GenerateHash() {
-        if (string.IsNullOrEmpty(this.Data))
-        {
-            this.hash = string.Empty;
-            
-            return;
+    private string SignData(string path, string verb, string data, string timestamp) {
+        if (string.IsNullOrEmpty(data)) {
+            return "";
         }
 
+        string meta = verb.ToLower()+":"+path+":"+timestamp;
+
         var encoding = new System.Text.UTF8Encoding();
-        byte[] keyByte = encoding.GetBytes(this.Secret);
-        byte[] messageBytes = encoding.GetBytes(this.Data);
+        byte[] keyByte = encoding.GetBytes(this.accessSecret);
+        byte[] messageBytes = encoding.GetBytes(data+meta);
 
         using (var hmacsha256 = new HMACSHA256(keyByte)) {
             byte[] hashMessage = hmacsha256.ComputeHash(messageBytes);
             var hexString = BitConverter.ToString(hashMessage);
-            // TODO
+            // By default, BitConverter's output is '-' separated hex pairs
             return hexString.Replace("-", "");
         }
     }

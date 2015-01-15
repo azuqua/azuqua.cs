34bd1d5c880e655de5bb7cbf4ddd7161c46a52b8
Lito Nicolai <lito@azuqua.com>
2015-01-15 13:30:14 -0800

    Refactor to a library structure

--- Azuqua/ConfigReader.cs
+++ Azuqua/ConfigReader.cs
@@ -12,37 +12,37 @@ namespace Azuqua
 
     /// <summary>
     /// This class is responsible for reading Flo Key and Secret from
-    /// system environment variables or application configuration file
+    /// system environment variables, or just take them on instantiation
     /// </summary>
     public class ConfigReader
     {
         /// <summary>
-        /// Flo's server URL environment variable name
-        /// </summary>
-        private const string FloServerUrlKeyVariableName = "floServerUrl";
-
-        /// <summary>
-        /// Flo's access key environment variable name
+        /// Flo access key environment var name
         /// </summary>
         private const string FloAccessKeyVariableName = "floAccessKey";
 
         /// <summary>
-        /// Flo's access secret environment variable name
+        /// Flo access secret environment var name
         /// </summary>
         private const string FloAccessSecretVariableName = "floAccessSecret";
 
         /// <summary>
         /// Initializes a new instance of the ConfigReader class
         /// </summary>
-        public ConfigReader()
+        public ConfigReader(string Key, string Secret)
         {
-            this.LoadConfigurationData();
+			this.Key = Key;
+			this.Secret = Secret;
         }
+		public ConfigReader()
+		{
+            this.LoadFromEnvVars();
+		}
 
         /// <summary>
-        /// Gets Flo Server API URL
+        /// Flo Server API URL
         /// </summary>
-        public string ServerUrl { get; private set; }
+        public const string ServerUrl = "api.azuqua.com";
 
         /// <summary>
         /// Gets Flo API key
@@ -57,31 +57,17 @@ namespace Azuqua
         /// <summary>
         /// Load Flo configuration data
         /// </summary>
-        private void LoadConfigurationData()
+        private void LoadFromEnvVars()
         {
-            if (File.Exists(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile) 
-                && ConfigurationManager.AppSettings.Count > 0
-                && !string.IsNullOrEmpty(ConfigurationManager.AppSettings[FloServerUrlKeyVariableName])
-                && !string.IsNullOrEmpty(ConfigurationManager.AppSettings[FloAccessKeyVariableName])
-                && !string.IsNullOrEmpty(ConfigurationManager.AppSettings[FloAccessSecretVariableName]))
-            {
-                this.ServerUrl = ConfigurationManager.AppSettings[FloServerUrlKeyVariableName];
-                this.Key = ConfigurationManager.AppSettings[FloAccessKeyVariableName];
-                this.Secret = ConfigurationManager.AppSettings[FloAccessSecretVariableName];
-            }
-            else if (!string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloServerUrlKeyVariableName))
-                    && !string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName))
+            if (!string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName))
                     && !string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloAccessSecretVariableName)))
             {
-                this.ServerUrl = System.Environment.GetEnvironmentVariable(FloServerUrlKeyVariableName);
                 this.Key = System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName);
                 this.Secret = System.Environment.GetEnvironmentVariable(FloAccessSecretVariableName);
             }
             else
             {
-                this.ServerUrl = string.Empty;
-                this.Key = string.Empty;
-                this.Secret = string.Empty;
+                throw new Exception("Missing floAccessKey or FloAccessSecret environment variables");
             }
         }
     }
--- Azuqua/MakeRequest.cs
+++ Azuqua/MakeRequest.cs
@@ -10,98 +10,105 @@ using System.Net;
 using System.Text;
 using System.IO;
 
-/// This class encapsulates all necessary data that need to make
-/// a request to Flo's API
-public class MainTest {
-    // TODO: TESTING ONLY (Lito)
-    static void Main(string[] args) {
-        // Display the number of command line arguments:
-        Azuqua azu = new Azuqua("key", "secret");
-    }
-}
+namespace Azuqua
+{
+/// This class encapsulates all necessary data that's needed to make
+/// a request to the Flo API
+    public class Azuqua 
+    {
+        private string accessKey, accessSecret;
 
-public class Azuqua {
-
-    private string accessKey, accessSecret;
-
-    /// Constructor
-    public Azuqua(string accessKey, string accessSecret) {
-        this.accessKey = accessKey;
-        this.accessSecret = accessKey;
-    }
-
-    public string InvokeFlo(string name, string alias, string data) {
-        string path = "/flo/"+alias+"/invoke";
-        string resp = this.MakeRequest(path, "POST", data);
-        return resp;
-    }
-
-    private string MakeRequest(string path, string verb, string data) {
-        if (data.Length < 1) {
-            data = "";
+        /// Constructor
+        public Azuqua(string accessKey, string accessSecret) {
+            this.accessKey = accessKey;
+            this.accessSecret = accessKey;
         }
-        //TODO: DOUBLE CHECK TIMESTAMP
-        string timestamp = DateTime.UtcNow.ToString("s"); // ISO formatted date
-        string hash = this.SignData(path, verb, data, timestamp);
 
-        verb = verb.ToUpper();
-        string host = "https://api.azuqua.com";
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host+path);
-        request.Method = verb;
-        request.ContentType = "application/json";
-        request.Headers.Add("x-api-accessKey", this.accessKey);
-        request.Headers.Add("x-api-timestamp", timestamp);
-        request.Headers.Add("x-api-hash", hash);
-
-        // Make it happen! 
-        if (verb == "GET") {
-            WebResponse response = request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            using (var streamReader = new StreamReader(responseStream)) {
-                string responseText = streamReader.ReadToEnd();
-                return responseText;        
+        public void InitFromEnvironmentVariables() {
+            if (!string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName))
+                    && !string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable(FloAccessSecretVariableName)))
+            {
+                this.accessKey = System.Environment.GetEnvironmentVariable(FloAccessKeyVariableName);
+                this.accessSecret = System.Environment.GetEnvironmentVariable(FloAccessSecretVariableName);
+            }
+            else
+            {
+                throw new Exception("Missing floAccessKey or FloAccessSecret environment variable");
             }
         }
-        else if (verb == "POST") {
-            // Let's take a second to appricate how cool the idea of streams is
-            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
-            request.ContentLength = dataBytes.Length;
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(dataBytes, 0, dataBytes.Length);
 
-            // .GetResponse both sends to the server and gets the response
-            WebResponse response = request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            using (var streamReader = new StreamReader(responseStream)) {
-                string responseText = streamReader.ReadToEnd();
-                return responseText;        
+        public string InvokeFlo(string name, string alias, string data) {
+            string path = "/flo/"+alias+"/invoke";
+            string resp = this.MakeRequest(path, "POST", data);
+            return resp;
+        }
+
+        private string MakeRequest(string path, string verb, string data) {
+            if (data.Length < 1) {
+                data = "";
             }
+            //TODO: DOUBLE CHECK TIMESTAMP
+            string timestamp = DateTime.UtcNow.ToString("s"); // ISO formatted date
+            string hash = this.SignData(path, verb, data, timestamp);
+
+            verb = verb.ToUpper();
+            string host = "https://api.azuqua.com";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host+path);
+            request.Method = verb;
+            request.ContentType = "application/json";
+            request.Headers.Add("x-api-accessKey", this.accessKey);
+            request.Headers.Add("x-api-timestamp", timestamp);
+            request.Headers.Add("x-api-hash", hash);
+
+            // Make it happen! 
+            if (verb == "GET") {
+                WebResponse response = request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                using (var streamReader = new StreamReader(responseStream)) {
+                    string responseText = streamReader.ReadToEnd();
+                    return responseText;        
+                }
+            }
+            else if (verb == "POST") {
+                byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+                request.ContentLength = dataBytes.Length;
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(dataBytes, 0, dataBytes.Length);
+
+                // .GetResponse both sends to the server and gets the response
+                WebResponse response = request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                using (var streamReader = new StreamReader(responseStream)) {
+                    string responseText = streamReader.ReadToEnd();
+                    return responseText;        
+                }
+            }
+            else {
+                // Should never get here- requests should be only GET or POST
+                throw new Exception("Azuqua only supports GET or POST calls");
+            }
+
         }
-        else {
-            // Should never get here- requests should be only GET or POST
-            return ""; 
-        }
 
-    }
+        /// Generate Hash-based Message Authentication Code (HMAC) with 
+        /// SHA256 and secret key
+        private string SignData(string path, string verb, string data, string timestamp) {
+            if (string.IsNullOrEmpty(data)) {
+                return "";
+            }
 
-    /// Generate Hash-based Message Authentication Code (HMAC) with 
-    /// SHA256 and secret key
-    private string SignData(string path, string verb, string data, string timestamp) {
-        if (string.IsNullOrEmpty(data)) {
-            return "";
-        }
+            string meta = verb.ToLower()+":"+path+":"+timestamp;
 
-        string meta = verb.ToLower()+":"+path+":"+timestamp;
+            var encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(this.accessSecret);
+            byte[] messageBytes = encoding.GetBytes(data+meta);
 
-        var encoding = new System.Text.UTF8Encoding();
-        byte[] keyByte = encoding.GetBytes(this.accessSecret);
-        byte[] messageBytes = encoding.GetBytes(data+meta);
-
-        using (var hmacsha256 = new HMACSHA256(keyByte)) {
-            byte[] hashMessage = hmacsha256.ComputeHash(messageBytes);
-            var hexString = BitConverter.ToString(hashMessage);
-            // By default, BitConverter's output is '-' separated hex pairs
-            return hexString.Replace("-", "");
+            using (var hmacsha256 = new HMACSHA256(keyByte)) {
+                byte[] hashMessage = hmacsha256.ComputeHash(messageBytes);
+                var hexString = BitConverter.ToString(hashMessage);
+                // By default, BitConverter's output is '-' separated hex pairs
+                return hexString.Replace("-", "");
+            }
         }
     }
 }
